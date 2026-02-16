fc13334b8a30dc65393c63782d538a56a6dd0b63
Markéta Srbová <srbova.ma.2022@skola.ssps.cz>
2026-02-16 11:48:35 +0100

    inverted index repository

--- backend/indexer/indexer.js
+++ backend/indexer/indexer.js
@@ -4,6 +4,7 @@ import { title } from "process";
 
 const dataContent = await fs.readFile("../data/raw/document.json", "utf-8");
 const pages = JSON.parse(dataContent);
+const invertedIndex = {};
 
 for (let i = 0; i < pages.length; i++){
     const dom = new JSDOM(pages[i].html);
@@ -14,16 +15,13 @@ for (let i = 0; i < pages.length; i++){
 
     let pureText = "";
     for (const el of document.querySelectorAll("p, h1, h2, h3, li")){
-        pureText += el?.textContent ?? "" + " "; // vrací text uvnitř elementu bez html tagů
+        pureText += (el?.textContent ?? "") + " "; // vrací text uvnitř elementu bez html tagů
     }
-    console.log(pureText);
 
     pureText = pureText.toLowerCase()
     .replace(/[^\p{L}\p{N}\s]/gu, "");
-    console.log(pureText);
 
-    const words = pureText.match(/\p{L}+/gu);
-    console.log(words);
+    const words = pureText.match(/\p{L}+/gu) ?? [];
 
     const wordFrequency = {};
     for(let word of words){
@@ -33,19 +31,29 @@ for (let i = 0; i < pages.length; i++){
             wordFrequency[word] = 1;
         }
     }
-    console.log(wordFrequency);
 
-    const invertedIndex = {};
+    for(let word in wordFrequency){
+        let tf = wordFrequency[word] / words.length;
 
-    for(let term in wordFrequency){
-        let tf = wordFrequency[term] / words.length;
-
-        if(!invertedIndex[term]){
-            invertedIndex[term] = [];
+        if(!invertedIndex[word]){
+            invertedIndex[word] = [];
         }        
 
-        invertedIndex[term].push({pageId: pageID, tf: tf});
+        invertedIndex[word].push({pageId: pageID, tf: tf});
     }
-
+    
+    }
+    for(let term in invertedIndex){
+        let idf = Math.log(pages.length / invertedIndex[term].length);
+        
+        for (let page of invertedIndex[term]) {
+            page.tfidf = page.tf * idf;
+            delete page.tf;
+        }
+    }
     console.log(invertedIndex);
-}
+
+    await fs.writeFile(
+        "../data/raw/inverted-index.json",
+        JSON.stringify(invertedIndex, null, 2), 
+        "utf-8");
